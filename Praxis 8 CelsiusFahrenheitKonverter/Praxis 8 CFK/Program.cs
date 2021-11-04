bacc91caf7a4ebe834eaf00eb68e68f0d97f098e
Knobs-Alpha <knobs.tv@gmail.com>
2021-11-04 08:29:07 +0100

    Praxis 8 Celsius in Fahrenheit Konverter

---
+++ Praxis 8 CelsiusFahrenheitKonverter/Praxis 8 CFK/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Praxis_8_CFK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abfrage zu Eingabe von Grad Celsius
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+
+            //Berechnung der eingabe von Celsius in Fahrenheit
+            double f = (Celsius * 9 / 5 + 32);
+
+            //Ausgabe der Eingabe von Celsius in fahrenheit
+            Console.WriteLine($"Es waren {Celsius} grad Celsius und das entsprechen {f} grad fahrenheit");
+        }
+    }
+}
