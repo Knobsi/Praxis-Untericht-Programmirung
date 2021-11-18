b45fc3879f78f9854e82878b410472634ef57ef4
Knobs-Alpha <leongiesselmann@gmail.com>
2021-11-18 09:43:15 +0100

    Praxis 11 & 12

---
+++ Praxis 11 Begrüßung/Praxis 11 Begrüßung/Praxis 11 Begrüßung/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Praxis_11_Begrüßung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name bitte! : ");
+            string name = Console.ReadLine();
+
+            Console.Write("vorname bitte! : ");
+            string vorname = Console.ReadLine();
+
+            Console.Write("Geschlecht in form von m oder w oder d bitte : ");
+            string geschlecht = Console.ReadLine();
+
+            if(geschlecht == "m" || geschlecht == "M")
+            {
+                Console.WriteLine($"Hallo Herr {name}!");
+            }
+            else if(geschlecht == "w" || geschlecht == "W")
+            {
+                Console.WriteLine($"Hallo Frau {name}!");
+            }
+            else if(geschlecht == "d" || geschlecht == "D")
+            {
+                Console.WriteLine($"Hallo {vorname} {name}");
+            }
+            else
+            {
+                Console.WriteLine("Enter drücken zum Neufersuch deine eingabe war nicht m,w,d");
+
+                string neustart = Console.ReadLine();
+
+
+                if (neustart == neustart)
+                {
+                    Console.Clear();
+                    Program.Main(args);
+                }
+
+            }
+        }
+    }
+}
---
+++ Praxis 12 Bedingte Division/Praxis 12 Bedingte Division/Praxis 12 Bedingte Division/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Praxis_12_Bedingte_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double summe;
+            double zwischenspeicher;
+
+            Console.Write("Zahl 1 Bittö :");
+            double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("zahl2 bittö :");
+            double zahl2 = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            if(zahl1 < zahl2)
+            {
+                summe = zahl1 / zahl2;
+
+                Console.WriteLine("Deine zahl 1 war" + zahl1);
+                Console.WriteLine("Deine zahl 2 war" + zahl2);
+                Console.WriteLine($"Das ergebnis aus {zahl1} durch { zahl2} lautet {summe:F} ");
+            }
+            else
+            {
+                zwischenspeicher = zahl1;
+                zahl1 = zahl2;
+                zahl2 = zwischenspeicher;
+
+
+                summe = zahl1 / zahl2;
+
+                Console.WriteLine("Deine zahl 1 war" + zahl1);
+                Console.WriteLine("Deine zahl 2 war" + zahl2);
+                Console.WriteLine($"Das ergebnis aus {zahl1} durch { zahl2} lautet {summe:F}");
+            }
+
+
+            
+
+        }
+    }
+}
