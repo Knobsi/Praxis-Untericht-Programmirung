e822e52a1fcc1be752f60b0f5f0a45f4dde76565
Knobs-Alpha <leongiesselmann@gmail.com>
2022-04-27 14:45:38 +0200

    praxis 43

---
+++ Praxis 43 Passwortgenerator/Praxis 43 Passwortgenerator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Praxis_43_Passwortgenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie bitte einen satz ein der zur erstellung ihres passwortes dient");
+            string satzFuerPasswort = Console.ReadLine();
+            string satzPasswort = GenerierePWAusSatz(satzFuerPasswort);
+            Console.WriteLine(satzPasswort);
+
+        }
+        public static string GenerierePWAusSatz(string satz)
+        {
+            string rueckgabe = "";
+            string[] bearbeitung = satz.Split(" ");
+            int laenge = 0;
+            if(bearbeitung.Length < 8)
+            {
+                foreach (string item in bearbeitung)
+                {
+                    
+                    rueckgabe += item.Substring(0,2);
+                    laenge++;
+
+                    char[] test = new char[item.Length];
+                    foreach(char c in test)
+                    {
+                        if (c == '.')
+                            rueckgabe += ".";
+                        else if (c == ',')
+                            rueckgabe += ",";
+                        else if (c == '-')
+                            rueckgabe += "-";
+                        else if (c == '!')
+                            rueckgabe += "!";
+                        else if (c == '?')
+                            rueckgabe += "?";
+                    }
+                }
+            }
+            else
+            {
+                foreach (string item in bearbeitung)
+                {
+
+                    rueckgabe += item.Substring(0,1);
+                    laenge++;
+
+                    char[] test = new char[item.Length];
+                    foreach (char c in test)
+                    {
+                        if(c == '.')
+                            rueckgabe += ".";
+                        else if(c ==',')
+                            rueckgabe += ",";
+                        else if(c =='-')
+                            rueckgabe += "-";
+                        else if(c =='!')
+                            rueckgabe += "!";
+                        else if (c == '?')
+                            rueckgabe += "?";
+                    }
+                }
+            }
+            rueckgabe += Convert.ToString(laenge);
+            return rueckgabe;
+        }
+    }
+}
