d73c077c670c736fd0d7f30fecc421c547ef54f5
Knobs-Alpha <leongiesselmann@gmail.com>
2022-04-28 07:29:05 +0200

    praxis 41 und 42

---
+++ Praxis 41 Chatbot/Praxis 41 Chatbot/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Praxis_41_Chatbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey wie gehts dir?");
+            string s1 = Console.ReadLine();
+            string[] gut = { "gut", "super", "supi", "geil", "ganz gut", "fein", "bestens" };
+
+            string[] schlecht = { "schlecht", "nicht gut", "scheiße", "nicht so gut", "kacke" };
+
+            for (int i = 0; i < gut.Length; i++)
+            {
+                bool g = s1.Contains(gut[i]);
+
+                if (g)
+                {
+                    Console.WriteLine($"Das freut mich das es dir {gut[i]} geht! Was hast du denn heute bisher so schönes gemacht?\n(bitte in ganzen Sätzen mit mehr als 5 Wörtern antworten!!)");
+                    Console.WriteLine($"Das eingabe wort {gut[i]} hast du ab zeichen {s1.LastIndexOf(gut[i])} verwendet");
+                    Console.WriteLine($"Das eingabe wort {gut[i]} hast du ab zeichen {s1.IndexOf(gut[i])} verwendet");
+                }
+            }
+
+            for (int i = 0; i < schlecht.Length; i++)
+            {
+                bool s = s1.Contains(schlecht[i]);
+
+                if (s)
+                {
+                    Console.WriteLine($"Das ist schade das es dir {schlecht[i]} geht! Was ist denn passiert?\n(bitte in ganzen Sätzen mit mehr als 5 Wörtern antworten!!)");
+                    Console.WriteLine($"Das eingabe wort {schlecht[i]} hast du ab zeichen {s1.IndexOf(schlecht[i])} verwendet");
+                }
+            }
+
+            string antwort = Console.ReadLine();
+            antwort = antwort.Remove(15);
+            Console.WriteLine("ZzzzZzzz...");
+            Console.WriteLine($"Boah ich hab nur aus Höflichkeit gefragt!! Mein Gott ey, nach \"{antwort}blablabla\" " +
+                $"habe ich dir gar nicht mehr zugehört");
+            Console.WriteLine("Du nervst echt, ich hoffe dir rutschen beim abspülen die Ärmel ins Wasser!!");
+
+            Console.WriteLine("(Beleidigung wie \"trottel\", \"idiot\" oder \"blöde Kuh\" wird erwartet...)");
+            string[] verboteneWoerter = new String[] { "trottel", "idiot", "blöde Kuh" };
+
+            antwort = Console.ReadLine();
+            string bla = "Tschüss!!";
+            for (int i = 0; i < verboteneWoerter.Length; i++)
+            {
+                if (antwort.Contains(verboteneWoerter[i]))
+                {
+                    antwort = antwort.Replace(verboteneWoerter[i], "*****");
+                    bla = $"Was soll den bitte {antwort} für eine Beleidigung sein? Bist du noch im Kindergarten?\n" +
+           $"Das wird mir jetzt echt zu blöd hier, Tschüss!!";
+                    break;
+                }
+            }
+            Console.WriteLine(bla);
+
+
+
+        }
+    }
+}
---
+++ Praxis 42 Buchstaben suchen und ersetzen/Praxis 42 Buchstaben suchen und ersetzen/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Praxis_42_Buchstaben_suchen_und_ersetzen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drei Chinesen mit dem Kontrabass saßen auf der Straße und erzählten sich was. Da kam die Polizei, fragt: "Was ist denn das?" Drei Chinesen mit dem Kontrabass.
+            Console.WriteLine("Gebe mir einen text den du endern und buschstaben zählen lassen willst");
+            string songText = Console.ReadLine();
+
+            Console.WriteLine("Wieviele buchstaben wollen sie suchen ?");
+            int wievielebuchstaben = Convert.ToInt32(Console.ReadLine());
+
+            int[] buchstabenanzahl = new int[wievielebuchstaben];
+            char[] charArray = new char[wievielebuchstaben];
+
+            for (int i = 0; i < buchstabenanzahl.Length; i++)
+            {
+                Console.WriteLine("Welchen buchstaben wollen sie suchen?");
+                char a = Convert.ToChar(Console.ReadLine());
+                charArray[i] = a;
+                buchstabenanzahl[i] = WieVielVonBustabe(a, songText);
+
+                Console.Clear();
+            }
+
+            Console.WriteLine("Durch welchen buchstaben wolen sie die Vokale austauschen?");
+
+            char tauscher = Convert.ToChar(Console.Read());
+            string newText = VertauscheBuchstaben(tauscher, songText);
+
+            Console.Clear();
+
+            for (int i = 0; i < buchstabenanzahl.Length; i++)
+            {
+                Console.WriteLine($"Der buchstabe {charArray[i]} kamm {buchstabenanzahl[i]} mal vor");
+            }
+
+            Console.WriteLine($"\nDer text sieht nun dachdem der buchstabe {tauscher} getauscht wurde wie folgt aus\n");
+            Console.WriteLine(newText);
+
+
+
+        }
+        static int WieVielVonBustabe(char a, string text)
+        {
+            int count;
+
+            text = text.ToLower();
+            a = Char.ToLower(a);
+
+            count = text.Count(c => c == a);
+            return count;
+        }
+        static string VertauscheBuchstaben(char a, string text)
+        {
+            text = text.Replace('a', a);
+            text = text.Replace('e', a);
+            text = text.Replace('i', a);
+            text = text.Replace('o', a);
+            text = text.Replace('u', a);
+            return text;
+        }
+    }
+}
